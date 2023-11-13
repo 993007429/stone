0165e5cbec908cb72d3bd8eaf3959ceb689a14f2
zhenfan.he <993007429@qq.com>
2023-11-13 15:48:34 +0800

    服务工厂

--- src/app/api/user.py
+++ src/app/api/user.py
@@ -5,9 +5,7 @@ from apiflask import APIBlueprint
 from marshmallow.fields import Integer
 
 from src.app.schema.user import PageQuery, UserIn, UserOut, UsersOut
-from src.modules.user.application.services import UserService
-from src.modules.user.infrastructure.repositories import SQLAlchemyUserRepository
-from src.modules.user.domain.services import UserDomainService
+from src.app.service_factory import AppServiceFactory
 
 user_blueprint = APIBlueprint('user', __name__, url_prefix='/users')
 
@@ -23,7 +21,7 @@ def login(query_data):
 @user_blueprint.input(UserIn, location='json')
 @user_blueprint.output(UserOut)
 def create_user(json_data):
-    res = UserService(UserDomainService(SQLAlchemyUserRepository())).create_user(**json_data)
+    res = AppServiceFactory.user_service.create_user(**json_data)
     return res.dict()
 
 
@@ -31,5 +29,5 @@ def create_user(json_data):
 @user_blueprint.input(PageQuery, location='query')
 @user_blueprint.output(UsersOut)
 def get_users(query_data):
-    res = UserService(UserDomainService(SQLAlchemyUserRepository())).get_users()
+    res = AppServiceFactory.user_service.get_users()
     return res.dict()
---
+++ src/app/service_factory.py
@@ -0,0 +1,54 @@
+from src.modules.user.application.services import UserService
+from src.modules.user.domain.services import UserDomainService
+from dependency_injector import containers, providers
+
+from src.modules.user.infrastructure.repositories import SQLAlchemyUserRepository
+
+
+class AIService:
+    def __init__(self, user_service: UserService):
+        self.user_service = user_service
+
+
+class UserContainer(containers.DeclarativeContainer):
+
+    repository = providers.Factory(
+        SQLAlchemyUserRepository
+    )
+
+    user_domain_service = providers.Factory(
+        UserDomainService,
+        repository=repository,
+    )
+
+    user_service = providers.Factory(
+        UserService,
+        domain_service=user_domain_service,
+    )
+
+
+class AIContainer(containers.DeclarativeContainer):
+
+    user_container = providers.DependenciesContainer()
+
+    ai_service = providers.Factory(
+        AIService,
+        user_service=user_container.user_service,
+    )
+
+
+class AppContainer(containers.DeclarativeContainer):
+
+    user_container = providers.Container(
+        UserContainer
+    )
+
+    ai_container = providers.Container(
+        AIContainer,
+        user_container=user_container
+    )
+
+
+class AppServiceFactory(object):
+    user_service: UserService = AppContainer.user_container.user_service()
+    ai_service: AIService = AppContainer.ai_container.ai_service()
