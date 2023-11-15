d7263939069336d1f2fddf0b3631cb762dff44fe
zhenfan.he <993007429@qq.com>
2023-11-15 18:37:27 +0800

    生成token

--- src/modules/user/utils/auth.py
+++ src/modules/user/utils/auth.py
@@ -17,7 +17,7 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
     return bcrypt.checkpw(plain_password.encode('utf-8'), hashed_password.encode('utf-8'))
 
 
-def get_token_for_user(user: UserEntity):
+def get_token_for_user(user: UserEntity) -> str:
     expired_at = int(time.time()) + setting.JWT_EXPIRE
     payload = {"userid": user.id, "exp": expired_at}
     return jwt.encode(payload, setting.SECRET_KEY, "HS256").decode('utf-8')
