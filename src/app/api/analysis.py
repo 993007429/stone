e0c73ebb7aebcd5d699572a2db261676b817c72d
zhenfan.he <993007429@qq.com>
2023-11-17 15:57:40 +0800

    12

--- src/app/api/analysis.py
+++ src/app/api/analysis.py
@@ -7,56 +7,56 @@ from marshmallow.fields import Integer
 from src.app.auth import auth_required
 from src.app.db import connect_db
 from src.app.permission import permission_required
-from src.app.schema.slice import StartIn, SingleStartOut, PollingIn, SinglePollingOut, CalculationIn, \
-    ListCalculationOut, SingleCalculationOut, ResultIn, SingleResultOut, CalculationQuery
+from src.app.schema.analysis import StartIn, SingleStartOut, PollingIn, SinglePollingOut, CalculationIn, \
+    ListAnalysesQuery, SingleCalculationOut, AnalysisIn, SingleAnalysisOut, AnalysesQuery
 from src.app.schema.user import PageQuery
 from src.app.service_factory import AppServiceFactory
 
-slice_blueprint = APIBlueprint('数据模块', __name__, url_prefix='/slices')
+analysis_blueprint = APIBlueprint('AI处理模块', __name__, url_prefix='/analysis')
 
 
-@slice_blueprint.post('/start')
-@slice_blueprint.input(StartIn, location='json')
-@slice_blueprint.output(SingleStartOut)
-@slice_blueprint.doc(summary='开始AI处理', security='ApiAuth')
+@analysis_blueprint.post('/start')
+@analysis_blueprint.input(StartIn, location='json')
+@analysis_blueprint.output(SingleStartOut)
+@analysis_blueprint.doc(summary='开始AI处理', security='ApiAuth')
 def start(json_data):
     res = AppServiceFactory.slice_service.start(**json_data)
     return res.response
 
 
-@slice_blueprint.post('/polling')
-@slice_blueprint.input(PollingIn, location='json')
-@slice_blueprint.output(SinglePollingOut)
-@slice_blueprint.doc(summary='轮询查看AI处理是否完成', security='ApiAuth')
+@analysis_blueprint.post('/polling')
+@analysis_blueprint.input(PollingIn, location='json')
+@analysis_blueprint.output(SinglePollingOut)
+@analysis_blueprint.doc(summary='轮询查看AI处理是否完成', security='ApiAuth')
 def polling(json_data):
     res = AppServiceFactory.slice_service.polling(**json_data)
     return res.response
 
 
-@slice_blueprint.get('/calculations')
-@slice_blueprint.input(CalculationQuery, location='query')
-@slice_blueprint.output(ListCalculationOut)
-@slice_blueprint.doc(summary='AI处理列表', security='ApiAuth')
-def get_calculations(query_data):
-    res = AppServiceFactory.slice_service.get_calculations(**query_data)
+@analysis_blueprint.get('')
+@analysis_blueprint.input(AnalysesQuery, location='query')
+@analysis_blueprint.output(ListAnalysesQuery)
+@analysis_blueprint.doc(summary='AI处理记录列表', security='ApiAuth')
+def get_analyses(query_data):
+    res = AppServiceFactory.slice_service.get_analyses(**query_data)
     return res.response
 
 
-@slice_blueprint.get('/calculations/{calculation_id}')
-@slice_blueprint.input(ResultIn, location='json')
-@slice_blueprint.output(SingleResultOut)
-@slice_blueprint.doc(summary='AI处理详情', security='ApiAuth')
-def get_calculation_result(json_data):
-    res = AppServiceFactory.slice_service.get_calculation_result(**json_data)
+@analysis_blueprint.get('/{analysis_id}')
+@analysis_blueprint.input(AnalysisIn, location='json')
+@analysis_blueprint.output(SingleAnalysisOut)
+@analysis_blueprint.doc(summary='AI处理详情', security='ApiAuth')
+def get_analysis(json_data):
+    res = AppServiceFactory.slice_service.get_analysis(**json_data)
     return res.response
 
 
-@slice_blueprint.delete('/calculations/{calculation_id}')
-@slice_blueprint.input(CalculationIn, location='json')
-@slice_blueprint.output(SingleCalculationOut)
-@slice_blueprint.doc(summary='AI处理列表', security='ApiAuth')
-def delete_calculation(json_data):
-    res = AppServiceFactory.slice_service.delete_calculation(**json_data)
+@analysis_blueprint.delete('/{analysis_id}')
+@analysis_blueprint.input(CalculationIn, location='json')
+@analysis_blueprint.output(SingleCalculationOut)
+@analysis_blueprint.doc(summary='删除AI处理记录', security='ApiAuth')
+def delete_analysis(json_data):
+    res = AppServiceFactory.slice_service.delete_analysis(**json_data)
     return res.response
 
 
--- src/app/schema/analysis.py
+++ src/app/schema/analysis.py
@@ -1,9 +1,9 @@
-from apiflask import Schema, PaginationSchema
-from apiflask.fields import Integer, String, List, Nested, DateTime
+from apiflask import Schema
+from apiflask.fields import Integer, String, Nested, DateTime
 from apiflask.validators import Range
 from apiflask.validators import Length, OneOf
 
-from src.app.schema.base import DurationField
+from src.app.custom_schema import DurationField
 from src.modules.slice.domain.value_objects import AiType
 
 
@@ -18,7 +18,7 @@ class StartIn(Schema):
 
 
 class StartOut(Schema):
-    calculation_id = Integer(required=True)
+    analysis_id = Integer(required=True)
 
 
 class SingleStartOut(Schema):
@@ -28,11 +28,11 @@ class SingleStartOut(Schema):
 
 
 class PollingIn(Schema):
-    calculation_id = Integer(required=True)
+    analysis_id = Integer(required=True)
 
 
 class PollingOut(Schema):
-    calculation_id = Integer(required=True)
+    analysis_id = Integer(required=True)
 
 
 class SinglePollingOut(Schema):
@@ -41,20 +41,20 @@ class SinglePollingOut(Schema):
     data = Nested(PollingOut)
 
 
-class CalculationQuery(Schema):
+class AnalysesQuery(Schema):
     page = Integer(load_default=1)
     per_page = Integer(load_default=10, validate=Range(max=100))
     userid = Integer(required=True)
 
 
 class CalculationIn(Schema):
-    calculation_id = Integer(required=True)
+    analysis_id = Integer(required=True)
 
 
 class CalculationOut(Schema):
     userid = Integer(required=True)
     username = String(required=True)
-    calculation_id = Integer(required=True)
+    analysis_id = Integer(required=True)
     slice_id = Integer(required=True)
     ai_model = String(required=True, validate=[OneOf([AiType.admin.value, AiType.user.value])])
     model_version = String(required=True, validate=[Length(8, 32)])
@@ -69,18 +69,18 @@ class SingleCalculationOut(Schema):
     data = Nested(CalculationOut)
 
 
-class ListCalculationOut(Schema):
+class ListAnalysesQuery(Schema):
     code = Integer(required=True)
     message = String(required=True)
     data = Nested(CalculationOut)
 
 
-class ResultIn(Schema):
-    calculation_id = Integer(required=True)
+class AnalysisIn(Schema):
+    analysis_id = Integer(required=True)
 
 
 class ResultOut(Schema):
-    calculation_id = Integer(required=True)
+    analysis_id = Integer(required=True)
     slice_id = Integer(required=True)
     ai_model = String(required=True, validate=[OneOf([AiType.admin.value, AiType.user.value])])
     model_version = String(required=True, validate=[Length(8, 32)])
@@ -93,7 +93,7 @@ class ResultOut(Schema):
     d = String(required=True, validate=[Length(0, 255)])
 
 
-class SingleResultOut(Schema):
+class SingleAnalysisOut(Schema):
     code = Integer(required=True)
     message = String(required=True)
     data = Nested(ResultOut)
