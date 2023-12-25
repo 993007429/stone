249c27fc338fa0979e905331297b2ad0d8e53eed
zhenfan.he <993007429@qq.com>
2023-12-25 10:05:24 +0800

    900

---
+++ commands/create_super_user.py
@@ -0,0 +1,28 @@
+import os
+import sys
+import argparse
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+
+class CreateUser:
+    def __init__(self):
+        self.parser = argparse.ArgumentParser(description='create user')
+        self.parser.add_argument('-u', '--username', help='username', default='sa')
+        self.parser.add_argument('-p', '--password', help='password', default='dyj123456')
+        self.parser.add_argument('-r', '--role', help='admin or user', default='admin')
+        self.parser.add_argument('-c', '--creator', help='creator', default='sa')
+
+    def run(self, args):
+        from stone.app.request_context import request_context
+        from stone.app.service_factory import AppServiceFactory
+        request_context.connect_db()
+        res = AppServiceFactory.user_service.create_user(username=args.username, password=args.password, role=args.role, creator=args.creator)
+        print(res.dict())
+        request_context.close_db()
+
+
+if __name__ == '__main__':
+    command = CreateUser()
+    args = command.parser.parse_args()
+    command.run(args)
--- scripts/create_super_user.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO `user` (`username`, `password_hash`, `role`)
-values ('sa', '$2b$12$9K.r42pNheVNSJAM70Cl0OwxiCSssvGnj9x/XE/gvAW/djs42Lq4i', 'admin');
